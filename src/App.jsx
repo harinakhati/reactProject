e2ca799c07c95d36bca088dfe3ebde71b9e987cf
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-09-06 23:48:34 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>}/>
+      <Route path="/about" element={<h1>About</h1>}/>
+      <Route path="/contact" element={<h1>Contact</h1>} />
+      
+      
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
